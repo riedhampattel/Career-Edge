96bf56b3bd4793fd21a59cd8cb69c406c7a72063
riedhampattel <riedhampattel.tops@gmail.com>
2024-10-05 12:27:42 +0530

    updated

---
+++ Python/22.py
@@ -0,0 +1,2 @@
+#Write a Python program to guess a number between 1 and 9.
+#Note : User is prompted to enter a guess. If the user guesses wrong then the prompt appears again until the guess is correct, on successful guess, user will get a "Well guessed!" message, and the program will exit.
---
+++ Python/23.py
@@ -0,0 +1,2 @@
+#Write a Python program to count the number of even and odd numbers in a series of numbers
+#Sample numbers : numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) 
