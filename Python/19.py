bc6a7c7112062231bb49af9537be775c411523c4
riedhampattel <riedhampattel.tops@gmail.com>
2024-10-05 12:18:59 +0530

    updated

---
+++ Python/19.py
@@ -0,0 +1,5 @@
+#Write a Python program that calculates the area of a circle based on the radius entered by the user.
+radius = float(input("Enter the radius of a circle = "))
+area = 3.14*(radius*radius)
+
+print(f"The area of a circle is = {area}")
---
+++ Python/21.py
@@ -0,0 +1,5 @@
+#Write a Python program to convert temperatures to and from Celsius and Fahrenheit.
+calcius = float(input("Enter the temperature in calcius = "))
+farenhit = (calcius*(9/5)) + 32
+
+print(f"Tenperature in farenhit is = {farenhit}")
