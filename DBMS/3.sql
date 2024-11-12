0666f6ddfe2c346b58f15549acba046438c7b28c
riedhampattel <riedhampattel.tops@gmail.com>
2024-11-12 13:24:03 +0530

    updated

--- DBMS/3.sql
+++ DBMS/3.sql
@@ -39,3 +39,45 @@ select * from employees;
 select * from employees where (manager_id,department_id) in ((124,50),(102,60));
 
 select * from employees where (manager_id,department_id) not in ((124,50),(102,60));
+
+select * from employees order by salary asc;
+
+select * from employees order by salary desc;
+
+select * from employees order by 5;
+
+select employee_id,first_name as name from employees order by name;
+
+select employee_id,first_name as name from employees;
+
+select * from employees where first_name like '%ee%';
+
+select * from employees where first_name like '%ee_';
+
+select * from employees where first_name like '%ee';
+
+select * from employees where first_name like '%e%e%';
+
+select * from employees where first_name like 'a%e';
+
+select * from employees where hire_date like '%-j_n-%';
+
+select * from employees where hire_date like '%-ju_-%';
+
+select min(salary) from employees;
+
+select max(salary) from employees;
+
+select sum(salary) from employees;
+
+select avg(salary) from employees;
+
+select count(commission_pct) from employees;
+
+select department_id,min(salary) from employees group by department_id;
+
+select department_id,count(*) from employees group by department_id;
+
+select distinct department_id from employees;
+
+select * from employees;
